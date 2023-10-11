00618921cf1274e8628e70744435ff2e9f59ec7b
Rupam Sutar <107876344+rupamsutar@users.noreply.github.com>
2023-10-11 15:31:22 +0530

    Generate a new access token using refresh token

--- src/services/token.service.js
+++ src/services/token.service.js
@@ -1,6 +1,24 @@
-import { sign } from "../utils/token.util.js";
+import createHttpError from "http-errors";
+import bcrypt from "bcryptjs";
+import UserModel from "../models/userModal.js";
+import { sign, verify } from "../utils/token.util.js";
 
-export const generateToken = async(payload, expires, secret ) => {
-    let token = sign(payload, expires, secret);
-    return token;
+export const generateToken = async (payload, expires, secret) => {
+  let token = sign(payload, expires, secret);
+  return token;
+};
+
+export const signUser = async (email, password) => {
+  const user = await UserModel.findOne({ email: email.toLowerCase() }).lean();
+  if (!user) throw createHttpError.NotFound("Invalid Credentials");
+
+  let passwordMatches = await bcrypt.compare(password, user.password);
+  if (!passwordMatches) throw createHttpError.NotFound("Invalid Credentials");
+
+  return user;
+};
+
+export const verifyToken = async(token, secret) => {
+    let check = await verify(token, secret);
+    return check
 }
